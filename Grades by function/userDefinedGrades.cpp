a0bb7fccd0a62d744fc6eb653d4fceb193bbe018
Noah Duran <46541422+noahd320@users.noreply.github.com>
2022-11-23 17:01:10 -0500

    Update userDefinedGrades.cpp

--- Grades by function/userDefinedGrades.cpp
+++ Grades by function/userDefinedGrades.cpp
@@ -1,6 +1,3 @@
-// userDefinedGrades.cpp -- This file will output a students grade for an assignment.
-//CSIS 111-<D02> 
-//Include statements 
 #include <iostream> 
 #include <string> 
 #include <cmath>
@@ -9,16 +6,18 @@ using namespace std;
 //Global declarations: Constants and type definitions only -- no variables
 //Function prototypes 
 
-
-double gradeCheck(double grade, double total);
-void nameCheck(string studentName);
+double gradeCheck(double grade, double total, ifstream&, ofstream&);
+void nameCheck(string name, ifstream& studentGrades, ofstream& calculatedGrades);
 
 
 int main()
 {
+    double grade, total, gradePercentage;
+    string names;
     ifstream studentGrades;
     ofstream calculatedGrades;
 
+    //Opening the input and output files
     studentGrades.open("studentGrades.txt");
     calculatedGrades.open("calculatedGrades.txt");
 
@@ -33,20 +32,22 @@ int main()
         exit(1);
     }
     
+    //This ensures that the code will go line by line until there are no more
     while (!studentGrades.eof())
     {
         string names;
         double grade, total, gradePercentage;
+        //Take in from studentGrades.txt and setting the variables that will be called by the functions
         studentGrades >> names >> grade >> total;
 
-        gradeCheck(grade, total);
+        nameCheck(names, studentGrades, calculatedGrades);
+        gradeCheck(grade, total, studentGrades, calculatedGrades);
+
     }
-
-
 
 //In cout statement below SUBSTITUTE  your name and lab number
     cout << "Noah Duran -- Lab 4" << endl << endl;
-
+    cout << "See the output file for table" << endl;
 
 //Variable declarations
 //Program logic
@@ -55,54 +56,47 @@ int main()
 return 0;
 }
 //Function definitions
-double gradeCheck(double grade, double total) {
-    ifstream studentGrades;
-    studentGrades.open("studentGrades.txt");
-    ofstream calculatedGrades;
-    calculatedGrades.open("calculatedGrades.txt");
+double gradeCheck(double grade, double total, ifstream& studentGrades, ofstream& calculatedGrades) {
 
-    string names;
     double gradePercentage;
     double gradeDecimal;
 
-    while (studentGrades >> names >> grade >> total)
-    {
-        gradePercentage = ceil((grade / total) * 100);
-        gradeDecimal = grade / total;
-        cout << names << '\n';
-        cout << gradePercentage << '\n';
-        cout << setprecision(5) << gradeDecimal << '\n';
-    }
+    //The percentage will round up and set it to a percentage value, decimal keeps it as decimal
+    gradePercentage = ceil((grade / total) * 100);
+    gradeDecimal = grade / total;
+
+    //Finding if the grade passes or not
+    //Setprecision ensures that you will only get two digits for percentage and 5 decimal value for the decimal portion
+    if (gradePercentage >= 90)
+        {
+            calculatedGrades << fixed << setprecision(0) << gradePercentage << "% " << fixed << setprecision(5) << gradeDecimal << " " << "Excellent" << '\n';
+        }
+    else if (gradePercentage >= 80)
+        {
+            calculatedGrades << fixed << setprecision(0) << gradePercentage << "% " << fixed << setprecision(5) << gradeDecimal << " " << "Well Done" << '\n';
+        }
+    else if (gradePercentage >= 70)
+        {
+            calculatedGrades << fixed << setprecision(0) << gradePercentage << "% " << fixed << setprecision(5) << gradeDecimal << " " << "Good" << '\n';
+        }
+    else if (gradePercentage >= 60)
+        {
+            calculatedGrades << fixed << setprecision(0) << gradePercentage << "% " << fixed << setprecision(5) << gradeDecimal << " " << "Need Improvement" << '\n';
+        }
+    else {
+            calculatedGrades << fixed << setprecision(0) << gradePercentage << "% " << fixed << setprecision(5) << gradeDecimal << " " << "Fail" << '\n';
+        }
 
     return gradePercentage;
     return gradeDecimal;
 
-
-    /*
-     if (gradePercentage >= 90)
-        {
-            calculatedGrades << names << " " << "Excellent" << '\n';
-        }
-    else if (gradePercentage >= 80)
-        {
-            calculatedGrades << names << " " << "Well Done" << '\n';
-        }
-    else if (gradePercentage >= 70)
-        {
-            calculatedGrades << names << " " << "Good" << '\n';
-        }
-    else if (gradePercentage >= 60)
-        {
-            calculatedGrades << names << " " << "Need Improvement" << '\n';
-        }
-    else {
-            calculatedGrades << names << " " << "Fail" << '\n';
-        }
-        */
-    
-
     }
 
-void nameCheck(ifstream& studentName) {
-    
+void nameCheck(string name, ifstream& studentGrades, ofstream& calculatedGrades) {
+
+    //Takes in the name and sets it to studentName, then out puts it to the file with a space
+    string studentName;
+    studentName = name;
+
+    calculatedGrades << studentName << " ";
 }
